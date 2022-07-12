319aa1966cff5c4f34e1d0b7d95a327100745be4
wilker123 <josewilkerlucena123@gmail.com>
2022-07-12 11:37:50 -0300

    created database configuration file

---
+++ schedule_app/lib/bd/database_helper.dart
@@ -0,0 +1,16 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DataBaseHelper {
+  static late var database;
+
+  Future<Database> getDB() async {
+    database = openDatabase(join(await getDatabasesPath(), 'bdcurso.db'),
+        onCreate: (db, version) {
+      return db.execute(
+        "CREATE TABLE contact(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, phone TEXT)",
+      );
+    }, version: 1);
+    return database;
+  }
+}
